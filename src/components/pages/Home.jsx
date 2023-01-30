a5ba5a21afb9927b08782d32e27539efe0202a84
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2023-01-30 03:17:44 -0600

    [add] - the Product detail was added and the section of category input was changed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,57 +1,83 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, InputGroup, Row, Form} from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  InputGroup,
+  Row,
+  Form,
+  Accordion,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { filterNewProductsThunk, filterProductsCategoryThunk, getProductsThunk } from "../../store/slices/products.slice";
+import { useNavigate } from "react-router-dom";
+import {
+  filterNewProductsThunk,
+  filterProductsCategoryThunk,
+  getProductsThunk,
+} from "../../store/slices/products.slice";
 
 const Home = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const productList = useSelector((state) => state.products);
+  const productList = useSelector((state) => state.products);
 
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    const [searchProduct, setSearchProduct] = useState("");
+  const [searchProduct, setSearchProduct] = useState("");
 
+  const navigate = useNavigate();
 
-    useEffect(() =>{
-        dispatch(getProductsThunk());
+  useEffect(() => {
+    dispatch(getProductsThunk());
 
-        axios.get('https://e-commerce-api-v2.academlo.tech/api/v1/categories')
-            .then(res => setCategories(res.data));
-    },[])
+    axios
+      .get("https://e-commerce-api-v2.academlo.tech/api/v1/categories")
+      .then((res) => setCategories(res.data));
+  }, []);
 
-    console.log(searchProduct);
+  console.log(searchProduct);
 
   return (
     <div>
-
-
-        <InputGroup className="mb-5" size="md">
+      <InputGroup className="mb-5" size="md">
         <Form.Control
           placeholder="Search product"
           aria-label="Search products"
           aria-describedby="basic-addon2"
           value={searchProduct}
-          onChange={e => setSearchProduct(e.target.value)}
+          onChange={(e) => setSearchProduct(e.target.value)}
         />
         <Button
-          onClick={() => dispatch(filterNewProductsThunk(searchProduct.toString()))}
+          onClick={() =>
+            dispatch(filterNewProductsThunk(searchProduct.toString()))
+          }
           variant="outline-secondary"
           id="button-addon2"
-        >Search</Button>
+        >
+          Search
+        </Button>
       </InputGroup>
-      
 
-      {categories.map((category) => (
-        <Button key={category.id} onClick={() => dispatch(filterProductsCategoryThunk(category.id))}>{category.name}</Button>
-      ))}
+      <Accordion defaultActiveKey="1">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Category filter</Accordion.Header>
+
+          {categories.map((category) => (
+            <Accordion.Body
+              key={category.id}
+              onClick={() => dispatch(filterProductsCategoryThunk(category.id))}
+            >
+              {category.name}
+            </Accordion.Body>
+          ))}
+        </Accordion.Item>
+      </Accordion>
 
       <ul>
         <Row xs={1} md={4} className="g-4">
           {productList.map((products) => (
-            <Col key={products.id}>
-              {/* <Col key={products.id} onClick={() => navigate(`/products/${products.id}`)}> */}
+            <Col key={products.id} onClick={() => navigate(`/product/${products.id}`)}>
 
               <Card style={{ width: "20rem" }}>
                 <Card.Img variant="center" src={products.images[0].url} />
@@ -60,9 +86,11 @@ const Home = () => {
                   <Card.Title>{products.title}</Card.Title>
                 </Card.Body>
                 <Card.Body>
-                  <Card.Title>Precio</Card.Title>
-                  <Card.Title>{products.price}</Card.Title> 
-                  <Button variant="primary" className="rounded-circle">Add</Button>
+                  <Card.Title>Price</Card.Title>
+                  <Card.Title>{products.price}</Card.Title>
+                  <Button variant="primary" className="rounded-circle">
+                    Add
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
--- src/components/pages/ProductsDetail.jsx
+++ src/components/pages/ProductsDetail.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Carousel } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 
 const ProductsDetail = () => {
-    return (
-        <div>
-            <h1>Producs detail</h1>    
-        </div>
-    );
+  const { id } = useParams();
+
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`)
+      .then((res) => setProduct(res.data));
+  }, []);
+
+  console.log(product);
+
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <p>{product.title}</p>
+
+      <Carousel>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={product.images?.[0]?.url}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={product.images?.[1]?.url}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={product.images?.[2]?.url}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        
+      </Carousel>
+
+      <h3>{product.brand}</h3>
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <h3>Price</h3>
+      <h3>{product.price}</h3>
+    
+    <Button>-</Button>
+    {/* <h3>{counter}</h3> */}
+    <Button>+</Button>
+
+    <Button>Add to car</Button>
+
+
+    </div>
+  );
 };
 
 export default ProductsDetail;
