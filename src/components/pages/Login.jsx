bfacc71c32e5ba856ae35c9c2a034b89dc72dbf3
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2023-01-30 20:26:49 -0600

    [add] - the form section was added and it works!

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,46 +1,65 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { setInputName } from "../../store/slices/inputName.slice";
+import { useForm } from "react-hook-form";
+import { Navigate, useNavigate } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { setInputName } from "../../store/slices/inputName.slice";
 
 const Login = () => {
+  // const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const {register,handleSubmit} = useForm();
 
-    const [ inputvalue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // const [inputvalue, setInputValue] = useState("");
 
+  // const navigate = useNavigate();
 
-    const clickButton = () => {
-        dispatch(setInputName(inputvalue));
-        navigate('/')
-    }
-    
+  // const clickButton = () => {
+  //   dispatch(setInputName(inputvalue));
+  //   navigate("/");
+  // };
+
+  const submit = (data) => {
+    console.log(data);
+    axios.post('https://e-commerce-api-v2.academlo.tech/api/v1/users/login/',data)
+      .then(res => {
+        localStorage.setItem('token', res.data.token);
+        navigate("/")
+      })
+      .catch(error =>{
+        if(error.response.status === 401)
+          alert("invalid credentials");
+        console.log(error);
+      })
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(submit)}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Text className="text-muted">
           Welcome! Enter your email and password to continue{" "}
         </Form.Text>
         <Form.Label>Email address</Form.Label>
-        <Form.Control 
-        type="email" 
-        placeholder="Enter email"
-        value={inputvalue}
-        onChange={e => dispatch(setInputValue(e.target.value))}
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          {...register("email")}
         />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control 
+        type="password" 
+        placeholder="Password" 
+        {...register("password")}
+        />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={clickButton}>
+      <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
